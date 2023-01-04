76ebd57c1533ebfa39baedab8e8b75f53fd1f467
ywbb <anastasia.karavdina@jda.com>
2023-01-04 20:50:22 +0100

    fix opencv test

--- driver/code/opencv_test.py
+++ driver/code/opencv_test.py
@@ -1,12 +1,19 @@
 import cv2
 
 def main():
-    camera = cv2.VideoCapture(-1)
-    camera.set(3, 640)
-    camera.set(4, 480)
-
-    while( camera.isOpened()):
-        _, image = camera.read()        
+    camera = cv2.VideoCapture(0) #build-in camera in Mac
+    #camera = cv2.VideoCapture(-1) #camera in Pi?
+    #camera.set(3, 640)
+    #camera.set(4, 480)
+    if not camera.isOpened():
+        print("Cannot open camera")
+        exit()
+    while True:
+        ret, image = camera.read()
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+            
         cv2.imshow('Original', image)
         
         b_w_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -14,7 +21,7 @@ def main():
         
         if cv2.waitKey(1) & 0xFF == ord('q') :
             break
-            
+    camera.release()
     cv2.destroyAllWindows()
     
 if __name__ == '__main__':
